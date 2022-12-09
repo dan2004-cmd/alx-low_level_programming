93207a53e7327609b03d0f8c43e2e8345f735ec9
dan2004-cmd <danielkapin2004@gmail.com>
2022-12-08 17:33:28 -0800

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  * Return: Always 0 (success)
  **/                                              
 int main(void)
-{                                                         puts("\"with proper grammar, but the outcome is a piece of art,");
+{                                                         printf("\"with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
