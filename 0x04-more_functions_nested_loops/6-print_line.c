aa5b0eadbfa564dfb7e0d2ed02b6307363ac3cae
dan2004-cmd <danielkapin2004@gmail.com>
2022-12-15 17:39:29 -0800

    a

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - print straight lines n times
+ * Return: no return
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
